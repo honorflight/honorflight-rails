481c5299c1a3641845da89f3c1a502ccb16db01f
Rob Brockschmidt <generelspaz@gmail.com>
2015-03-14 14:30:11 -0500

    updated war date for year only

--- app/admin/war.rb
+++ app/admin/war.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register War do
+  actions :all, :except => [:destroy]
   permit_params :name, :abbreviation, :begin_year, :end_year
 
   menu parent: "Reference Data"
@@ -6,6 +7,27 @@ ActiveAdmin.register War do
   filter :name, label: 'Name'
   filter :abbreviation, label: 'Abbreviation'
 
+  index do
+    selectable_column
+    column :name
+    column :abbreviation
+    column :begin_year
+    column :end_year
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :abbreviation
+      f.input :begin_year, as: :select, collection: 1935...2000
+      f.input :end_year, as: :select, collection: 1935...2000
+    end
+
+    f.actions do
+      f.action :submit
+    end
+  end
 
 
 
--- app/models/war.rb
+++ app/models/war.rb
@@ -2,7 +2,7 @@ class War < ActiveRecord::Base
 	has_many :people
 
   def with_years(attr)
-    self[attr] + " (#{self.begin_year.strftime('%Y')}-#{self.end_year.strftime('%Y')})"
+    self[attr] + " (#{self.begin_year}-#{self.end_year})"
   end
 
   def name_with_years
@@ -16,4 +16,21 @@ class War < ActiveRecord::Base
   def as_json(options={})
     super(only: [:id, :name, :description], methods: [:name_with_years, :abbreviation_with_years])
   end
+
+	def begin_year
+		self[:begin_year].strftime('%Y') unless self[:begin_year].blank?
+	end
+
+	def end_year
+		self[:end_year].strftime('%Y') unless self[:end_year].blank?
+	end
+
+	def begin_year=(year)
+		self[:begin_year] = Date.parse("#{year}-01-01")
+	end
+
+	def end_year=(year)
+		self[:end_year] = Date.parse("#{year}-01-01")
+	end
+
 end
