7d72572479fa537ae64c9b1eeff328883ce0ef8f
Rob Brockschmidt <generelspaz@gmail.com>
2015-03-19 23:42:40 -0500

    moved services awards to ref data menu

--- app/admin/service_awards.rb
+++ app/admin/service_awards.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register ServiceAward do
 permit_params :name
+menu parent: "Reference Data"
 # menu false
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
