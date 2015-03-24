55da41965643c601fedc092045d2b6d748248f58
Jeff Ancel <jancel@gmail.com>
2015-03-23 22:08:36 -0500

    adding rob's fix to remove the double remove, but only one remove

--- app/assets/javascripts/aa.js
+++ app/assets/javascripts/aa.js
@@ -1,3 +1,6 @@
-$(window).ready(function(){
-  $('[id*=address_attributes_zipcode_input]').siblings().find('.button.has_many_remove').hide()
+
+$(function(){
+  $(document).on("click", "a.has_many_add", function(){
+    $('[id*=address_attributes_zipcode_input]').siblings().find('.button.has_many_remove').hide();
+  });
 });
