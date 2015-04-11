e5b7aa2f4544361e6ca6abc3e92e29f53cc358dc
Jeff Ancel <jancel@gmail.com>
2015-04-11 13:30:26 -0500

    changing encryption key to env variable

--- app/models/medical_condition_type.rb
+++ app/models/medical_condition_type.rb
@@ -1,6 +1,7 @@
 class MedicalConditionType < ActiveRecord::Base
   attr_encrypted :name, key: ENV['ENCRYPTION_KEY_MEDICALCONDITIONNAME']
 
+
 	has_many :medical_condition_names
 	has_many :medical_conditions
 
