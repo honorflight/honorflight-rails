7e790759c9a3b7ba6361acf651f9ebd73cfa5795
Rob Brockschmidt <generelspaz@gmail.com>
2015-05-01 22:07:10 -0500

    added drop down filtering on load to veteran on load

--- app/assets/javascripts/aa.js
+++ app/assets/javascripts/aa.js
@@ -1,4 +1,4 @@
-
+//hide add new button
 $(function(){
   $(document).on("click", "a.has_many_add", function(){
     $('[id*=address_attributes_zipcode_input]').siblings().find('.button.has_many_remove').hide();
@@ -6,68 +6,78 @@ $(function(){
 });
 
 
+
+//filter medical conditions
 $(function(){
-  $(document).on("change", "select.medical_condition_type_dd", function(){
-    var $select = $(this);
-    var $nameSelect = $select.parent("li").siblings("li").find(".medical_condition_name_dd");
-    var type_id = $select.val();
+  $(document).on("change", "select.medical_condition_type_dd", filterMedicalConditions);
+  $(document).on("change", "select.branch_dd,select.rank_type_dd", filterBranchRank);
 
-    var narrowMedicalNameList = function(data, select, nameSelect){
-      $names = $(nameSelect);
-      $names.val("");
-      $names.children("option").attr("style", "display:none");
-      $names.children("option").each(function(){
-        // data and look through it for it's there, remove style
-        for(var i = 0; i < data.length; i = i + 1){
-          if (parseInt($(this).val()) == data[i].id){
-            $(this).removeAttr("style")
-          }
-        }
-      });
-    };
-
-    $.get("/admin/medical_condition_names.json?utf8=%E2%9C%93&q%5Bmedical_condition_type_id_eq%5D=" + type_id + "&commit=Filter&order=id_desc", "", function(data){ narrowMedicalNameList(data, $select, $nameSelect); }, "json");
-  });
+  filterMedicalConditions("select.medical_condition_type_dd");
+  filterBranchRank("select.branch_dd");
 });
 
 
+var filterMedicalConditions = function(selector){
+  var $select = null;
+  if (typeof selector === "object") {
+    $select = $(this);
+  } else {
+    $select = $(selector);
+  }
 
-$(function(){
-  var narrowList = function(values, select){
-    $names = $(select);
+  var $nameSelect = $select.parent("li").siblings("li").find(".medical_condition_name_dd");
+  var type_id = $select.val();
+
+  $.get("/admin/medical_condition_names.json?utf8=%E2%9C%93&q%5Bmedical_condition_type_id_eq%5D=" + type_id + "&commit=Filter&order=id_desc", "", function(data){ narrowList(data, $nameSelect); }, "json");
+}
+
+
+var filterBranchRank = function(selector){
+  var $select = null;
+  if (typeof selector === "object") {
+    $select = $(this);
+  } else {
+    $select = $(selector);
+  }
+
+  var $nameSelect = $select.parent("li").siblings("li").find(".rank_dd");
+
+  if ($select.hasClass("branch_dd")){
+    var branchId = $select.val();
+    var rankTypeId = $select.parent("li").siblings("li").find(".rank_type_dd").val();
+  } else {
+    var branchId = $select.parent("li").siblings("li").find(".branch_dd").val();
+    var rankTypeId = $select.val();
+  }
+
+  $.get("/admin/ranks.json?q%5Brank_type_id_eq%5D="+ rankTypeId +"&q%5Bbranch_id_eq%5D=" + branchId, "", function(data){  narrowList(data, $nameSelect); });
+};
+
+
+var narrowList = function(data, select){
+  $names = $(select);
+  var found = false;
+
+  for (var i = 0; i < data.length; i++){
+    var name = data[i];
+    if (parseInt($names.val()) === name.id){
+      found = true;
+    } 
+  }
+  if (!found){
     $names.val("");
-    $names.children("option").attr("style", "display:none");
-    $names.children("option").each(function(){
-      // data and look through it for it's there, remove style
-      for(var i = 0; i < values.length; i = i + 1){
-        if (parseInt($(this).val()) == values[i].id){
-          $(this).removeAttr("style")
-        }
+  }
+  $names.children("option").attr("style", "display:none");
+  $names.children("option").each(function(){
+    // data and look through it for it's there, remove style
+    for(var i = 0; i < data.length; i = i + 1){
+      if (parseInt($(this).val()) == data[i].id){
+        $(this).removeAttr("style")
       }
-    });
-  };
-
-  $(document).on("change", "select.rank_type_dd", function(){});
-
-
-  $(document).on("change", "select.branch_dd,select.rank_type_dd", function(){
-    var $nameSelect = $(this).parent("li").siblings("li").find(".rank_dd");
-
-    var $dd = $(this);
-
-    if ($dd.hasClass("branch_dd")){
-      var branchId = $dd.val();
-      var rankTypeId = $(this).parent("li").siblings("li").find(".rank_type_dd").val();
-    } else {
-      var branchId = $(this).parent("li").siblings("li").find(".branch_dd").val();
-      var rankTypeId = $dd.val();
     }
-
-    $.get("/admin/ranks.json?q%5Brank_type_id_eq%5D="+ rankTypeId +"&q%5Bbranch_id_eq%5D=" + branchId, "", function(data){ 
-      narrowList(data, $nameSelect); 
-    });
   });
-});
+};
+
 
 //    /admin/ranks.json?utf8=✓&q%5Brank_type_id_eq%5D=1&q%5Bbranch_id_eq%5D=1
 
