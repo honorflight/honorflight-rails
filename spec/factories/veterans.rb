409dcd2bef715c91c5c373bf5c3118b673b17c7e
achernyak <artemchernyak@gmail.com>
2015-05-01 22:45:10 -0500

    added suffix

--- spec/factories/veterans.rb
+++ spec/factories/veterans.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :veteran, aliases: [:person] do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+    name_suffix { 1 }
     middle_name { Faker::Name.name }
     email { Faker::Internet.email }
     work_email { Faker::Internet.email }
