7ae3d17c428c4724665250bc5fb300ed464cd6f2
Jeff Ancel <jancel@gmail.com>
2015-05-04 21:08:14 -0500

    beginning pdf workings (trying to make the routes respond to generate a report on all the people type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,14 @@ class ApplicationController < ActionController::Base
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
+
+  # Needed for sessions controller and some admin functionality
+  def authenticate_admin_user!
+    logger.debug "Trying to authenticate through APIKEY #{headers['HTTP_X_ADMIN_APIKEY']}"
+    if request.headers['HTTP_X_ADMIN_APIKEY'].present?
+      redirect_to new_admin_user_session_path unless AdminUser.authenticate_by_apikey(request.headers['HTTP_X_ADMIN_APIKEY'])
+    else
+      super
+    end
+  end
 end
