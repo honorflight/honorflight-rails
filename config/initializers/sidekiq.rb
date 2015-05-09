4fd3d24a237540980e07a183b8ef43454ab2ec5f
Jeff Ancel <jancel@gmail.com>
2015-05-09 17:36:47 -0500

    configuring more sidekiq

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,18 @@
+redis_url = ENV["REDIS_URL"] || 'redis://localhost:6379'
+database_url = ENV['DATABASE_URL'] || 'postgres://localhost:5432?pool=25'
+
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: redis_url
+  }
+
+  ActiveRecord::Base.establish_connection if database_url
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: redis_url
+  }
+
+  ActiveRecord::Base.establish_connection if database_url
+end
