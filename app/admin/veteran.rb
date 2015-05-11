8b0cd2c8c35f8b5f37e6b79d010cd8e821caa4c1
Jeff Ancel <jancel@gmail.com>
2015-05-10 20:44:36 -0500

    fixing medical condition name #fail

--- app/admin/veteran.rb
+++ app/admin/veteran.rb
@@ -177,7 +177,7 @@ ActiveAdmin.register Veteran do
       panel "Medical Conditions" do
         table_for veteran.medical_conditions do
           column("Condition Type") { |condition| condition.medical_condition_type.name }
-          column("Condition Name") { |condition| condition.medical_condition_name.name }
+          column("Condition Name") { |condition| condition.try(:medical_condition_name, :name) }
           column :last_occurrence
           column("Comments") { |condition| condition.comment }
         end
