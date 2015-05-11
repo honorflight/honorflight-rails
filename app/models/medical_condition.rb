aae69e37424ccc1336ec90ae3e7a84b5019ac1ba
Jeff Ancel <jancel@gmail.com>
2015-05-11 00:02:02 -0500

    fix up medical condition

--- app/models/medical_condition.rb
+++ app/models/medical_condition.rb
@@ -1,12 +1,19 @@
 class MedicalCondition < ActiveRecord::Base
   attr_encrypted :diagnosed_at,  key: ENV["ENCRYPTION_KEY_MEDICALCONDITION"], marshal: true, marshaler: Marshel::Date
-  attr_encrypted :last_occurrence, key: ENV["ENCRYPTION_KEY_MEDICALCONDITION"]
+  attr_encrypted :last_occurrence, key: ENV["ENCRYPTION_KEY_MEDICALCONDITION"], marshal: true, marshaler: Marshel::Date
   attr_encrypted :comment, key: ENV["ENCRYPTION_KEY_MEDICALCONDITION"]
   # attr_encrypted :diagnosed_last, :description, key: 'future key alg', marshal: true, marshaler: Marshel::Date
 
   belongs_to :person
   belongs_to :medical_condition_type
   belongs_to :medical_condition_name
+
+  before_validation :set_last_occurrence
+  def set_last_occurrence
+    if self.last_occurrence.blank?
+      self.last_occurrence=Date.today
+    end
+  end
 
   def last_occurrence=(d)
     self[:encrypted_last_occurrence] = MedicalCondition.encrypt_last_occurrence(d)
