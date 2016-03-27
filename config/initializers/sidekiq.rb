0575121cacc5022558fcd78c6c21bf228ef2ff4a
Jeff Ancel <jancel@gmail.com>
2016-03-26 20:43:30 -0500

    changing sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,17 +3,31 @@ redis_password = ENV["REDISCLOUD_PASSWORD"] || nil
 database_url = ENV['DATABASE_URL'] || 'postgres://localhost:5432?pool=25'
 
 Sidekiq.configure_server do |config|
-  config.redis = {
-    url: redis_url
-  }
-  config.redis[:password] = redis_password if redis_password
+  unless redis_password then
+    config.redis = {
+      url: redis_url
+    }
+  else
+    config.redis = {
+      url: redis_url,
+      password: redis_password
+    }
+  end
+
   ActiveRecord::Base.establish_connection if database_url
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {
-    url: redis_url
-  }
-  config.redis[:password] = redis_password if redis_password
+  unless redis_password then
+    config.redis = {
+      url: redis_url
+    }
+  else
+    config.redis = {
+      url: redis_url,
+      password: redis_password
+    }
+  end
+
   ActiveRecord::Base.establish_connection if database_url
 end
