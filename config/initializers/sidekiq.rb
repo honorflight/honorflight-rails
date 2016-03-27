61d527df96c5e8651498030afa088f8a3b6c0ac4
Jeff Ancel <jancel@gmail.com>
2016-03-26 20:33:52 -0500

    adding redis password

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,13 @@
 redis_url = ENV["REDISCLOUR_URL"] || ENV["REDIS_URL"] || 'redis://localhost:6379'
+redis_password = ENV["REDISCLOUD_PASSWORD"] || nil
 database_url = ENV['DATABASE_URL'] || 'postgres://localhost:5432?pool=25'
 
 Sidekiq.configure_server do |config|
   config.redis = {
     url: redis_url
   }
+
+  config.redis[:password] = redis_password if redis_password
 
   ActiveRecord::Base.establish_connection if database_url
 end
