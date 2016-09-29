3ff412f09dfa220d45a15533ea5abf2f6c603879
Jeff Ancel <jancel@gmail.com>
2016-09-29 18:55:54 -0500

    removing batch actions

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -13,6 +13,7 @@ ActiveAdmin.setup do |config|
   # for each of the active admin pages.
   #
   config.site_title = "Honorflight Rails"
+  config.batch_actions = false
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
